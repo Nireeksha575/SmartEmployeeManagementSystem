ec1ec231002b04dfb0b2be8916ae8ffda83fc8ff
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-04-23 11:25:39 +0530

    repository created

---
+++ SmartEmployeeManagementSystem/src/main/java/com/example/smartemployeemanagementsystem/SmartEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartemployeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmployeeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEmployeeManagementSystemApplication.class, args);
+    }
+
+}
---
+++ SmartEmployeeManagementSystem/src/test/java/com/example/smartemployeemanagementsystem/SmartEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartemployeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
